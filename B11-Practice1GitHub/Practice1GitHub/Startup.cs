10961cdab5330be7422a3452fa0e366982f4c4d9
jadhavbalaji123@gmail.com <jadhavbalaji123@gmail.com>
2020-08-21 08:44:53 +0530

    changes in Layout.cshtml page and add title to the page

---
+++ B11-Practice1GitHub/Practice1GitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice1GitHub.Startup))]
+namespace Practice1GitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
